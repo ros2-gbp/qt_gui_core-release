d037724f94ae6d981f2f12ab89571b7060091a47
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-05-19 09:52:20 +0200

    Revert "Trimmed the branch to only the qt_gui sub directory"
    This reverts commit d0a1e676079702bba293f618d05d9a4a0c596273.

---
+++ qt_dotgraph/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+package_name = 'qt_dotgraph'
+
+setup(
+    name=package_name,
+    version='2.10.0',
+    packages=[package_name],
+    package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Thibault Kruse',
+    maintainer='Chris Lalancette',
+    maintainer_email='clalancette@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'qt_dotgraph provides helpers to work with dot graphs.'
+    ),
+    license='BSD',
+    tests_require=['pytest'],
+)
