5d770a2a5636585db67bb5265c3fa0f8215f3425
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-05-06 16:33:51 +0200

    Imported upstream version '2.11.0' of 'upstream'

--- qt_dotgraph/setup.py
+++ qt_dotgraph/setup.py
@@ -4,7 +4,7 @@ package_name = 'qt_dotgraph'
 
 setup(
     name=package_name,
-    version='3.0.0',
+    version='2.11.0',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
